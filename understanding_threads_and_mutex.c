adf2fb905d38f4e02f011d532bf26820ea9e0e16
carl0sfelipe <37373901+carl0sfelipe@users.noreply.github.com>
2023-09-29 22:26:25 -0300

    Update understanding_threads_and_mutex.c

--- understanding_threads_and_mutex.c
+++ understanding_threads_and_mutex.c
@@ -1,45 +1,69 @@
-
+// Standard library headers for input/output, string manipulation, memory allocation, etc.
 #include <stdio.h>
 #include <pthread.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 
+// Global counter that threads will increment or decrement.
 int counter = 0;
+
+// Mutex to protect access to the shared 'counter' variable.
 pthread_mutex_t counter_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+// Variable to control when threads should stop.
 int running = 1;
 
+// Arrays to indicate which threads are paused.
 int pause_increment[10] = {0};
 int pause_decrement[10] = {0};
+
+// Arrays to store thread IDs.
 pthread_t threads_inc[10], threads_dec[10];
+
+// Counters for number of threads.
 int num_threads_inc = 0, num_threads_dec = 0;
 
+// Thread function to increment the counter.
 void *increment(void *arg) {
+    // Get the thread's index.
     int idx = *(int *)arg - 1;
+
+    // Keep running until signaled to stop.
     while (running) {
+        // If this thread is paused, sleep for a second and check again.
         while (pause_increment[idx]) {
             sleep(1);
         }
+
+        // Lock the mutex, increment the counter, then unlock.
         pthread_mutex_lock(&counter_mutex);
         counter++;
         pthread_mutex_unlock(&counter_mutex);
-        sleep(1);
+
+        sleep(1);  // Sleep for a second before repeating.
     }
-    free(arg);
+
+    free(arg);  // Free the dynamically allocated memory for the index.
     return NULL;
 }
 
+// Thread function to decrement the counter.
 void *decrement(void *arg) {
     int idx = *(int *)arg - 1;
+
     while (running) {
         while (pause_decrement[idx]) {
             sleep(1);
         }
+
         pthread_mutex_lock(&counter_mutex);
         counter--;
         pthread_mutex_unlock(&counter_mutex);
+
         sleep(1);
     }
+
     free(arg);
     return NULL;
 }
@@ -47,7 +71,9 @@ void *decrement(void *arg) {
 int main() {
     char command[50];
 
+    // Main loop to accept commands from the user.
     while (1) {
+        // Print available commands.
         printf("\nCommands:\n");
         printf("ci - Create incrementing thread\n");
         printf("cd - Create decrementing thread\n");
@@ -58,9 +84,12 @@ int main() {
         printf("s - Show counter status\n");
         printf("e - Exit\n");
         printf("Enter a command: ");
-        fgets(command, 50, stdin);
-        strtok(command, "\n");
 
+        // Read user input.
+        fgets(command, 50, stdin);
+        strtok(command, "\n");  // Remove newline.
+
+        // Handle user commands.
         if (strncmp(command, "ci", 2) == 0 && strlen(command) == 2) {
             if (num_threads_inc < 10) {
                 int *idx = malloc(sizeof(int));
